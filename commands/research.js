111f85ce9d3da98cea2954c6f305053f861e3dac
Jaku-learning <franrod2004@gmail.com>
2025-02-06 15:16:10 -0500

    Deleted extra files and fixed error with the eslint file

--- commands/research.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { EmbedBuilder } = require('discord.js');
-
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('research')
-		.setDescription('Research technology in an easy way!'),
-};
